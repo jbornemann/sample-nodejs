e9dfa915dd890c7501cacdb35a3b5c876a5cb98c
Jeff Bornemann <jeff.bornemann@i-cubed.com>
2017-02-15 17:11:31 -0500

    Dockerfile added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, resp) {
-    res.send('Hello World\n');
+    resp.send('Hello World\n');
 });
 
 app.listen(8080);
